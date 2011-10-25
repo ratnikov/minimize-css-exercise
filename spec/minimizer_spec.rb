0e11f8ab46fdc52e203ec2e8801e8fcf9ad4874a
Dmitry Ratnikov <ratnikov@google.com>
2011-10-25 11:48:21 -0400

    Rewrote the specs a bit.

--- spec/minimizer_spec.rb
+++ spec/minimizer_spec.rb
@@ -2,107 +2,46 @@ require 'spec_helper'
 
 describe Minimizer do
   it "should remove blank lines" do
-    content =
-      '
-      #wrapper {
-       background: #CCC;
-      }
+    content = <<-CONTENT
+foo
+baz
 
-      #container {
-       width: 990px;
-       margin: 0px auto;
-       background: #FFF;
-      }
-      '
+bar
 
-    minimized_content =
-      '#wrapper {
-       background: #CCC;
-      }
-      #container {
-       width: 990px;
-       margin: 0px auto;
-       background: #FFF;
-      }'
 
-    Minimizer.minimize(content).should == minimized_content
+
+zeta
+    CONTENT
+
+    Minimizer.minimize(content).should == "foo\nbaz\nbar\nzeta"
   end
 
-  it "should remove lines that consist of comments" do
-    content =
-      '/* reset a few things */
-      body {
-       margin: 0;
-       padding: 0;
-      }'
+  it "should remove comments" do
+    content = <<-CONTENT
+foo
+/* this is a new line comment
 
-    minimized_content =
-      'body {
-       margin: 0;
-       padding: 0;
-      }'
+bar /* with a side comment */
 
-    Minimizer.minimize(content).should == minimized_content
-  end
+/**
+ * multiline comment!
+ */
+baz
+    CONTENT
 
-  it "should remove inline comments" do
-    content =
-      '/* reset a few things */
-      body {
-       margin: 0; /* or maybe 10px? */
-       padding: 0; /* or maybe 20px? will think about that tomorrow */
-      }'
-
-    minimized_content =
-      'body {
-       margin: 0; 
-       padding: 0; 
-      }'
-
-    Minimizer.minimize(content).should == minimized_content
-  end
-
-  it "should remove both blank lines and comments" do
-    content =
-      '
-      /* color of brain-matter */
-      #wrapper {
-       background: #CCC;
-      }
-
-      #container {
-       width: 990px;
-       margin: 0px auto;
-       background: #FFF;
-      }
-      '
-
-    minimized_content =
-      '#wrapper {
-       background: #CCC;
-      }
-      #container {
-       width: 990px;
-       margin: 0px auto;
-       background: #FFF;
-      }'
-
-    Minimizer.minimize(content).should == minimized_content
+    Minimizer.minimize(content).should == "foo\nbar\baz"
   end
 
   it "should return empty string for a string that consists only of blank lines and comments" do
-    content =
-      '
+    content = <<-CONTENT
       /* @todo: define styles for body */
 
       /* @todo: define styles for header */
       /* @todo: define styles for footer */
 
       /* @todo: define styles for main menu */
-      '
+    CONTENT
 
-    minimized_content = ''
-
-    Minimizer.minimize(content).should == minimized_content
+    Minimizer.minimize(content).should == ""
   end
 end
